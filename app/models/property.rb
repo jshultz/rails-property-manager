34450afe93cde4773fd05f326359340225000996
Jason Shultz <jasshultz@gmail.com>
2014-06-29 10:30:17 -0600

    more image stuff

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,7 +2,7 @@ class Property < ActiveRecord::Base
   belongs_to :user
   has_many :tickets
 
-  has_attached_file :property_photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :property_photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.jpg"
   validates_attachment_content_type :property_photo, :content_type => /\Aimage\/.*\Z/
 
 end
